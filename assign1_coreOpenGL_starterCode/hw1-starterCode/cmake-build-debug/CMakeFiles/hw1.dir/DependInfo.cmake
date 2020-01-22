e893b44d433f3bdea5a7d33e5270d08d1eb089c1
XunZhan <zhanxun100@163.com>
2020-01-21 21:38:05 -0800

    add assginment1

---
+++ assign1_coreOpenGL_starterCode/hw1-starterCode/cmake-build-debug/CMakeFiles/hw1.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/xun/Documents/USC/Class/CSCI-420-Computer-Graphics/assign1/assign1_coreOpenGL_starterCode/external/imageIO/imageIO.cpp" "/Users/xun/Documents/USC/Class/CSCI-420-Computer-Graphics/assign1/assign1_coreOpenGL_starterCode/hw1-starterCode/cmake-build-debug/CMakeFiles/hw1.dir/Users/xun/Documents/USC/Class/CSCI-420-Computer-Graphics/assign1/assign1_coreOpenGL_starterCode/external/imageIO/imageIO.cpp.o"
+  "/Users/xun/Documents/USC/Class/CSCI-420-Computer-Graphics/assign1/assign1_coreOpenGL_starterCode/openGLHelper-starterCode/basicPipelineProgram.cpp" "/Users/xun/Documents/USC/Class/CSCI-420-Computer-Graphics/assign1/assign1_coreOpenGL_starterCode/hw1-starterCode/cmake-build-debug/CMakeFiles/hw1.dir/Users/xun/Documents/USC/Class/CSCI-420-Computer-Graphics/assign1/assign1_coreOpenGL_starterCode/openGLHelper-starterCode/basicPipelineProgram.cpp.o"
+  "/Users/xun/Documents/USC/Class/CSCI-420-Computer-Graphics/assign1/assign1_coreOpenGL_starterCode/openGLHelper-starterCode/openGLMatrix.cpp" "/Users/xun/Documents/USC/Class/CSCI-420-Computer-Graphics/assign1/assign1_coreOpenGL_starterCode/hw1-starterCode/cmake-build-debug/CMakeFiles/hw1.dir/Users/xun/Documents/USC/Class/CSCI-420-Computer-Graphics/assign1/assign1_coreOpenGL_starterCode/openGLHelper-starterCode/openGLMatrix.cpp.o"
+  "/Users/xun/Documents/USC/Class/CSCI-420-Computer-Graphics/assign1/assign1_coreOpenGL_starterCode/openGLHelper-starterCode/pipelineProgram.cpp" "/Users/xun/Documents/USC/Class/CSCI-420-Computer-Graphics/assign1/assign1_coreOpenGL_starterCode/hw1-starterCode/cmake-build-debug/CMakeFiles/hw1.dir/Users/xun/Documents/USC/Class/CSCI-420-Computer-Graphics/assign1/assign1_coreOpenGL_starterCode/openGLHelper-starterCode/pipelineProgram.cpp.o"
+  "/Users/xun/Documents/USC/Class/CSCI-420-Computer-Graphics/assign1/assign1_coreOpenGL_starterCode/hw1-starterCode/hw1.cpp" "/Users/xun/Documents/USC/Class/CSCI-420-Computer-Graphics/assign1/assign1_coreOpenGL_starterCode/hw1-starterCode/cmake-build-debug/CMakeFiles/hw1.dir/hw1.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_CONSOLE"
+  "_DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../external/glm"
+  "../../external/imageIO"
+  "../../openGLHelper-starterCode"
+  "../../external/jpeg-9a-mac/include"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk/System/Library/Frameworks/OpenGL.framework"
+  "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.15.sdk/System/Library/Frameworks/GLUT.framework/Headers"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
